3c707da2f071b02f1b247b2614ef374203a42b6d
Jacob Zimmerman <jacobz_20@yahoo.com>
2018-01-08 09:14:34 -0600

    Combining both repositories

---
+++ epoch/option.py
@@ -0,0 +1,44 @@
+class Optional:
+    def __init__(self):
+        raise TypeError("Cannot instantiate Optional directly. Use Optional.empty, Optional.full(), or Option.ofNullable()")
+
+    class Empty:
+        def map(self, trans):
+            return self
+
+        def orElse(self, value):
+            return value
+
+    class Some:
+        def __init__(self, value):
+            self.value = value
+
+        def map(self, trans):
+            return Optional.ofNullable(trans(self.value))
+
+        def orElse(self):
+            return self.value
+
+    empty = Empty()
+
+    @staticmethod
+    def full(value):
+        return None
+
+    @staticmethod
+    def ofNullable(value):
+        if value is None:
+            return Optional.empty
+        else:
+            return Optional.full(value)
+
+
+class DictOption:
+    def __init__(self, map):
+        self.map = map
+
+    def __getitem__(self, item):
+        try:
+            return Optional.ofNullable(self.map[item])
+        except KeyError:
+            return Optional.empty
---
+++ epoch/rounding.py
@@ -0,0 +1,5 @@
+# Starting Data:
+#
+
+# TODO: Create a Sequence so I can use the sequence/stream APIs as methods instead of normal
+# ugly functions.
